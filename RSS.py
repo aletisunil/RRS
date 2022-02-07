c0dc18b85efe1eb33b9f641290c5702d00c7163c
Sunil Aleti <aletisunil1997@gmail.com>
2022-02-07 06:55:33 -0600

    Add files via upload

--- RSS.py
+++ RSS.py
@@ -22,7 +22,7 @@ class SampleApp(tk.Tk):
         
 
         self.frames = {}
-        for F in (StartPage, PageOne, PageTwo,PageThree,PageFour,PageFive):
+        for F in (StartPage, PageOne, PageTwo,PageThree,PageFour,PageFive,PageSix,PageSeven):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -33,7 +33,7 @@ class SampleApp(tk.Tk):
         self.show_frame("StartPage")
 
     def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
+        
         frame = self.frames[page_name]
         frame.tkraise()
 
@@ -46,22 +46,28 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Railway Reservation System", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Retrive Info by Name",
+        button1 = tk.Button(self, text="Retrive Info by Name",width=25,
                             command=lambda: controller.show_frame("PageOne"))
-        button2 = tk.Button(self, text="Retrive Info by Age",
+        button2 = tk.Button(self, text="Retrive Info by Age",width=25,
                             command=lambda: controller.show_frame("PageTwo"))
-        button3 = tk.Button(self, text="Retrive no.of passengers",
+        button3 = tk.Button(self, text="Retrive no.of passengers",width=25,
                             command=lambda: controller.show_frame("PageThree"))
-        button4 = tk.Button(self, text="Retrive bookings by date",
+        button4 = tk.Button(self, text="Retrive bookings by date",width=25,
                             command=lambda: controller.show_frame("PageFour"))
-        button5 = tk.Button(self, text="Retrive bookings by Train name",
+        button5 = tk.Button(self, text="Retrive bookings by Train name",width=25,
                             command=lambda: controller.show_frame("PageFive"))
+        button6 = tk.Button(self, text="Make a reservation",width=25,
+                            command=lambda: controller.show_frame("PageSix"))
+        button7 = tk.Button(self, text="Cancel a reservation",width=25,
+                            command=lambda: controller.show_frame("PageSeven"))
+        button1.pack(side='top',pady=8)
+        button2.pack(side='top',pady=8)
+        button3.pack(side='top',pady=8)
+        button4.pack(side='top',pady=8)
+        button5.pack(side='top',pady=8)
+        button6.pack(side='top',pady=8)
+        button7.pack(side='top',pady=8)
         
-        button1.pack()
-        button2.pack()
-        button3.pack()
-        button4.pack()
-        button5.pack()
         
 
 class PageOne(tk.Frame):
@@ -264,25 +270,14 @@ class PageFour(tk.Frame):
         Label1.pack(side="top")
         Entry1=tk.Entry(self)
         Entry1.pack(side="top")
-
-        
-
         cursor = my_conn.cursor()
-        
-        
-        
         def delete():
             self.tree.destroy()   
-        
-        
-
         def infoDay():
             query=("select * from Passenger where DOJ='{0}' and status='confirmed';").format(Entry1.get())
             cursor.execute(query)
             myresult = cursor.fetchall()
             print(myresult)
-            
-            
             res = [list(ele) for ele in myresult]
             if myresult!=None:
                 columns = ('firstName', 'lastName', 'Age','Address','status','TrainNum','DOJ','category')
@@ -300,10 +295,8 @@ class PageFour(tk.Frame):
                     self.tree.insert('', tk.END, values=i)
         
             else:
-                
                 self.tree = ttk.Treeview(self)
                 self.tree.pack()
-                
                 self.tree.insert('', 'end', text=" No Records found here ")
            
         button3=tk.Button(self, text="Submit",command=lambda:infoDay())
@@ -337,28 +330,17 @@ class PageFive(tk.Frame):
         ]
         
         
-        # datatype of menu text
+       
         clicked = tk.StringVar()
-        
-        # initial menu text
         clicked.set( "AUHO" )
         
         # Create Dropdown menu
         drop = tk.OptionMenu( self , clicked , *options )
         drop.pack()
-        
-        
-        
-
-        
-        
         cursor = my_conn.cursor()
         
         def delete():
             self.tree.destroy()   
-        
-        
-        
         def infoTrainName():
             query=("SELECT Passenger.firstName, Passenger.lastName, Passenger.Age,Passenger.Address, Passenger.DOJ, Passenger.category FROM Passenger INNER JOIN Train ON Passenger.TrainNum=Train.TrainNum where Train.TrainName='{0}' and Passenger.status='confirmed';").format(clicked.get())
             cursor.execute(query)
@@ -394,6 +376,229 @@ class PageFive(tk.Frame):
         button = tk.Button(self, text="Home Page",command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
+class PageSix(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Make a reservation", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        
+        Label1=tk.Label(self, text="First name")
+        Label1.pack(side="top")
+        Entry1=tk.Entry(self)
+        Entry1.pack(side="top")
+
+        Label2=tk.Label(self, text="Last name")
+        Label2.pack(side="top")
+        Entry2=tk.Entry(self)
+        Entry2.pack(side="top")
+
+        Label3=tk.Label(self, text="Age")
+        Label3.pack(side="top")
+        Entry3=tk.Entry(self)
+        Entry3.pack(side="top")
+
+        Label4=tk.Label(self, text="Address")
+        Label4.pack(side="top")
+        Entry4=tk.Entry(self)
+        Entry4.pack(side="top")
+        """
+        Label5=tk.Label(self, text="status")
+        Label5.pack(side="top")
+        Entry5=tk.Entry(self)
+        Entry5.pack(side="top")
+        """
+        Label6=tk.Label(self, text="TrainNum")
+        Label6.pack(side="top")
+        options1 = [
+            "456",
+            "789",
+            "123",
+            "111",
+            "222"
+        ] 
+        # datatype of menu text
+        clicked1 = tk.StringVar()
+        
+        # initial menu text
+        clicked1.set("456")
+        
+        # Create Dropdown menu
+        drop1 = tk.OptionMenu( self , clicked1 , *options1 )
+        drop1.pack(side='top')
+
+        Label7=tk.Label(self, text="DOJ")
+        Label7.pack(side="top")
+        Entry7=tk.Entry(self)
+        Entry7.pack(side="top")
+
+        Label8=tk.Label(self, text="category")
+        Label8.pack(side="top")
+        options2 = [
+            "General",
+            "Premium"
+        ]
+        
+        
+        # datatype of menu text
+        clicked2 = tk.StringVar()
+        
+        # initial menu text
+        clicked2.set("General")
+        
+        # Create Dropdown menu
+        drop2 = tk.OptionMenu( self , clicked2 , *options2 )
+        drop2.pack(side='top')
+       
+        def booking():
+            cursor = my_conn.cursor()
+            querry=("select count(*) from Passenger where TrainNum='{0}' and category='{1}';").format(int(clicked1.get()),clicked2.get())
+            cursor.execute(querry)
+            result = cursor.fetchall()
+            seats=int(result[0][0])
+        
+            if seats<=10:
+                temp='Confirmed'
+            else:    
+                temp='Waiting'
+            query="INSERT INTO Passenger values(%s,%s,%s,%s,%s,%s,%s,%s)"
+            value=(Entry1.get(),Entry2.get(),Entry3.get(),Entry4.get(),temp,int(clicked1.get()),Entry7.get(),clicked2.get())
+            cursor.execute(query, value)
+            my_conn.commit()
+
+            print(cursor.rowcount, "record inserted.")
+            query=("select * from Passenger")
+            cursor.execute(query)
+            myresult = cursor.fetchall()
+            print(myresult)
+            res = [list(ele) for ele in myresult]
+            if myresult!=None:
+                columns = ('firstName', 'lastName', 'Age','Address','status','TrainNum','DOJ','category')
+                self.tree = ttk.Treeview(self, columns=columns, show='headings')
+                self.tree.pack()
+                self.tree.heading('firstName', text='First Name')
+                self.tree.heading('lastName', text='Last Name')
+                self.tree.heading('Age', text='Age')
+                self.tree.heading('Address', text='Address')
+                self.tree.heading('status', text='Status')
+                self.tree.heading('TrainNum', text='Train Number')
+                self.tree.heading('DOJ', text='Date of Journey')
+                self.tree.heading('category', text='Category')
+                for i in res:
+                    self.tree.insert('', tk.END, values=i)
+        
+            else:
+                self.tree = ttk.Treeview(self)
+                self.tree.pack()
+                self.tree.insert('', 'end', text=" No Records found here ")
+        
+        def delete():
+            self.tree.destroy()    
+        button3=tk.Button(self, text="Submit",command=lambda:booking())
+        button3.pack(side='top')
+        button4=tk.Button(self, text="clear",command=lambda:delete())
+        button4.pack(side='top')
+        button = tk.Button(self, text="Home Page",command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageSeven(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Cancel a ticket", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        
+        Label1=tk.Label(self, text="First name")
+        Label1.pack(side="top")
+        Entry1=tk.Entry(self)
+        Entry1.pack(side="top")
+
+        Label2=tk.Label(self, text="Last name")
+        Label2.pack(side="top")
+        Entry2=tk.Entry(self)
+        Entry2.pack(side="top")
+
+        Label6=tk.Label(self, text="TrainNum")
+        Label6.pack(side="top")
+        options1 = [
+            "456",
+            "789",
+            "123",
+            "111",
+            "222"
+        ] 
+        # datatype of menu text
+        clicked1 = tk.StringVar()
+        
+        # initial menu text
+        clicked1.set("456")
+        
+        # Create Dropdown menu
+        drop1 = tk.OptionMenu( self , clicked1 , *options1 )
+        drop1.pack(side='top')
+
+        Label7=tk.Label(self, text="DOJ")
+        Label7.pack(side="top")
+        Entry7=tk.Entry(self)
+        Entry7.pack(side="top")
+
+       
+        def cancel():
+            cursor = my_conn.cursor()
+            sql = ("DELETE FROM Passenger where firstName='{0}' and lastName='{1}' and trainNum='{2}' and DOJ='{3}';").format(Entry1.get(),Entry2.get(),clicked1.get(),Entry7.get())
+            cursor.execute(sql)
+            my_conn.commit()
+            print(cursor.rowcount, "record(s) deleted")
+            """
+            result = cursor.fetchall()
+            seats=int(result[0][0])
+        
+            if seats<=10:
+                temp='Confirmed'
+            else:    
+                temp='Waiting'
+            query="INSERT INTO Passenger values(%s,%s,%s,%s,%s,%s,%s,%s)"
+            value=(Entry1.get(),Entry2.get(),Entry3.get(),Entry4.get(),temp,int(clicked1.get()),Entry7.get(),clicked2.get())
+            cursor.execute(query, value)
+            my_conn.commit()
+
+            print(cursor.rowcount, "record inserted.")
+            query=("select * from Passenger")
+            cursor.execute(query)
+            myresult = cursor.fetchall()
+            print(myresult)
+            res = [list(ele) for ele in myresult]
+            if myresult!=None:
+                columns = ('firstName', 'lastName', 'Age','Address','status','TrainNum','DOJ','category')
+                self.tree = ttk.Treeview(self, columns=columns, show='headings')
+                self.tree.pack()
+                self.tree.heading('firstName', text='First Name')
+                self.tree.heading('lastName', text='Last Name')
+                self.tree.heading('Age', text='Age')
+                self.tree.heading('Address', text='Address')
+                self.tree.heading('status', text='Status')
+                self.tree.heading('TrainNum', text='Train Number')
+                self.tree.heading('DOJ', text='Date of Journey')
+                self.tree.heading('category', text='Category')
+                for i in res:
+                    self.tree.insert('', tk.END, values=i)
+        
+            else:
+                self.tree = ttk.Treeview(self)
+                self.tree.pack()
+                self.tree.insert('', 'end', text=" No Records found here ")
+        """
+        def delete():
+            self.tree.destroy()    
+        button3=tk.Button(self, text="Submit",command=lambda:cancel())
+        button3.pack(side='top')
+        button4=tk.Button(self, text="clear",command=lambda:delete())
+        button4.pack(side='top')
+        button = tk.Button(self, text="Home Page",command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+        
 if __name__ == "__main__":
     app = SampleApp()
     app.mainloop()
